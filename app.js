34979ecf7c6d6f5295d1cb74ecfafcb46e1efc26
Adarsh nm <adarshnm02@gmail.com>
2024-03-11 09:46:41 +0530

    Implement socket, user count and connection management

--- app.js
+++ app.js
@@ -4,4 +4,23 @@ const app = express()
 const PORT = process.env.PORT || 4000
 const server = app.listen(PORT, ()=> console.log(`Server on port ${PORT}`))
 
+const io = require('socket.io')(server)
+
 app.use(express.static(path.join(__dirname, 'public')))
+
+let socketsConected = new Set()
+
+io.on('connection', onConnected)
+
+
+function onConnected(socket){
+    console.log('Socket connected', socket.id)
+  socketsConected.add(socket.id)
+  io.emit('clients-total', socketsConected.size)
+
+  socket.on('disconnect', () => {
+    console.log('Socket disconnected', socket.id)
+    socketsConected.delete(socket.id)
+    io.emit('clients-total', socketsConected.size)
+  })
+}
